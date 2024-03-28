c3ca0aaf980e82fbb6e9e4807b64af63e3f47b4a
hyunki0523 <159873023+hyunki0523@users.noreply.github.com>
2024-03-28 09:52:19 +0900

    [level 0] Title: 0 떼기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.py
@@ -0,0 +1,12 @@
+def solution(n_str):
+    answer = ''
+    
+    for i in n_str:
+        if i == '0':
+            pass
+        else :
+            j = n_str.index(i)
+            break
+
+    answer= n_str[j:]
+    return answer
